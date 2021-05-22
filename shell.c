2a60482a942ad424e72ddc813176898d8bed911a
2hyes <2hyes.com@gmail.com>
2021-05-22 22:57:31 +0900

    [debug]background 프로세스로 한 번 실행한 이후, 계속 bg로 처리되는 오류 수정

--- shell.c
+++ shell.c
@@ -6,6 +6,10 @@
 #include <fcntl.h>
 #define MAXARG 7
 
+int isBackground = 0;
+
+void command_exec(char *args[]);
+
 int main() {
     char buf[256];
     char *args[MAXARG];
@@ -15,6 +19,8 @@ int main() {
     static const char delim[] = " \t\n";
     int pid, status;
 
+    int mode = 0;
+    
     while(1) {
         printf("[shell] "); /* 프롬프트 출력 */
         gets(buf);
@@ -23,6 +29,9 @@ int main() {
         s = strtok_r(buf, delim, &save); /* 문자열에서 delim을 기준으로 단어를 잘라냄 */
 
         while(s) {
+            if (strcmp(s, "&") == 0) { // (2) 후면 실행
+                isBackground = 1;
+            }
             args[argn++] = s;
             s = strtok_r(NULL, delim, &save);
         }
@@ -31,15 +40,28 @@ int main() {
             continue;
         if (!strcmp(args[0], "quit")) // 프롬프트 종료
             break;
-
-        if ( (pid = fork()) == -1 ) // fork 호출에 실패
-            perror("fork failed");
-        else if (pid != 0) { // 부모 프로세스 - 자식 프로세스의 종료 wait
-            pid = waitpid(pid, &status, 0);
-        }
-        else { // 자식 프로세스 - execvp를 이용하여 arg[0] 실행
-            execvp(args[0], args);
-        }
+        
+        if (mode == 0)
+            command_exec(args);
     }
     exit(0);
+}
+
+void command_exec(char *args[]) {
+    int status, pid;
+    
+    if ( (pid = fork()) == -1 ) // fork 호출에 실패하면
+        perror("fork failed");
+    else if (pid != 0) { // 부모 - 자식 프로세스의 종료 wait
+        if ( isBackground ) {
+            printf("[1] %d\n", getpid());
+            waitpid(pid, &status, WNOHANG);
+            isBackground = 0;
+        }
+        else
+            pid = waitpid(pid, &status, 0);
+    }
+    else { // 자식 - execvp를 이용하여 arg[0] 실행
+        execvp(args[0], args);
+    }
 }
